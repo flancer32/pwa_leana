c0f3f7850a737e27e81d13d20d87da80a9f4b2db
Alex Gusev <alex@flancer64.com>
2020-10-13 17:18:00 +0300

    WF-397 Vue i18n

--- src/Front/App.mjs
+++ src/Front/App.mjs
@@ -1,5 +1,18 @@
+const i18next = self.i18n.i18next;
+i18next.addResources('en', 'app', {
+    msg: 'You clicked me {{ count }} times.',
+    lang: 'Change language.',
+});
+i18next.addResources('ru', 'app', {
+    msg: 'Ты кликнул по мне {{ count }} раз.',
+    lang: 'Сменить язык.',
+});
+
 const template = `
-<button v-on:click="count++">You clicked me {{ count }} times.</button>
+<div>
+<button v-on:click="count++">{{ $t("app:msg", {count}) }}</button>
+<button v-on:click="changeLang">{{ $t("app:lang") }}</button>
+</div>
 `;
 
 export default function Fl32_Leana_Front_App(spec) {
@@ -10,6 +23,14 @@ export default function Fl32_Leana_Front_App(spec) {
             return {
                 count: 0
             };
-        }
+        },
+        methods: {
+            changeLang() {
+                const current = this._i18n.i18next.language;
+                const next = (current === 'en') ? 'ru' : 'en';
+                this._i18n.i18next.changeLanguage(next);
+            }
+        },
+        i18n: self.i18n
     };
 }
