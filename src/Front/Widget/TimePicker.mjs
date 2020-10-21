5dba85a759adfb1bf1d2aaa4f45ae06df2ac0283
Alex Gusev <alex@flancer64.com>
2020-10-21 12:17:03 +0300

    LEAN-21 Improve DateTime util

--- src/Front/Widget/TimePicker.mjs
+++ src/Front/Widget/TimePicker.mjs
@@ -22,39 +22,11 @@ const template = `
 </div>
 `;
 
-/**
- *  Convert minutes to 'hours:minutes'.
- *
- * @param {Number} mins
- * @returns {string}
- */
-function convertMinsToHrsMins(mins) {
-    let h = Math.floor(mins / 60);
-    let m = mins % 60;
-    h = h < 10 ? '0' + h : h;
-    m = m < 10 ? '0' + m : m;
-    return `${h}:${m}`;
-}
-
-/**
- * Convert 'hours:minutes' to minutes.
- *
- * @param {string} hm 10:00
- * @returns {number} 600
- */
-function convertHrsMinsToMins(hm) {
-    let result;
-    if (hm.includes(':')) {
-        const [h, m] = hm.split(':');
-        result = Number(h) * 60 + Number(m);
-    } else {
-        result = Number(hm);
-    }
-    return result;
-}
-
 export default function Fl32_Leana_Front_Widget_TimePicker(spec) {
+    /** @type {Fl32_Leana_Front_Widget_TimePicker_Entry} */
     const entry = spec.Fl32_Leana_Front_Widget_TimePicker_Entry$;
+    /** @type {Fl32_Leana_Shared_Util_DateTime} */
+    const util = spec.Fl32_Leana_Shared_Util_DateTime$;
     return {
         template,
         components: {
@@ -74,12 +46,12 @@ export default function Fl32_Leana_Front_Widget_TimePicker(spec) {
              */
             entries() {
                 let result = [];
-                const beginMins = convertHrsMinsToMins(this.begin);
-                const endMins = convertHrsMinsToMins(this.end);
-                const step = convertHrsMinsToMins(this.step);
+                const beginMins = util.convertHrsMinsToMins(this.begin);
+                const endMins = util.convertHrsMinsToMins(this.end);
+                const step = util.convertHrsMinsToMins(this.step);
                 let id = 1;
                 for (let time = beginMins; (time + step) <= endMins; time += step) {
-                    result.push({id: id++, label: convertMinsToHrsMins(time)});
+                    result.push({id: id++, label: util.convertMinsToHrsMins(time)});
                 }
                 return result;
             }
--- src/Shared/Util/DateTime.mjs
+++ src/Shared/Util/DateTime.mjs
@@ -12,8 +12,12 @@ export default class Fl32_Leana_Shared_Util_DateTime {
      * @returns {string}
      */
     convertMinsToHrsMins(mins) {
-        let h = Math.floor(mins / 60);
-        let m = mins % 60;
+        let h = 0;
+        let m = 0;
+        if (mins) {
+            h = Math.floor(mins / 60);
+            m = mins % 60;
+        }
         h = h < 10 ? '0' + h : h;
         m = m < 10 ? '0' + m : m;
         return `${h}:${m}`;
@@ -26,14 +30,14 @@ export default class Fl32_Leana_Shared_Util_DateTime {
      * @returns {number} 600
      */
     convertHrsMinsToMins(hm) {
-        let result;
+        let result = 0;
         if (
             (typeof hm === 'string') &&
             (hm.includes(':'))
         ) {
             const [h, m] = hm.split(':');
             result = Number(h) * 60 + Number(m);
-        } else {
+        } else if (hm) {
             result = Number(hm);
         }
         return result;
